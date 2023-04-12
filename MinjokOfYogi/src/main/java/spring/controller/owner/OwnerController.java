cabaec63eb916e0dff8d4feb54d4e5198f37a456
BoHyun Choi <126458406+lambdaramge@users.noreply.github.com>
2023-04-12 17:46:43 +0900

    사장님페이지 초기설정2_0412

--- MinjokOfYogi/src/main/java/spring/controller/owner/OwnerController.java
+++ MinjokOfYogi/src/main/java/spring/controller/owner/OwnerController.java
@@ -1,5 +1,13 @@
 package spring.controller.owner;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OwnerController {
 
+	@GetMapping("owner-join")
+	public String join() {
+		return "owner/join";
+	}
 }
